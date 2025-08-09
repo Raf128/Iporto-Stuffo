83c88c4f31c49ba9e2d1080a068f6c2ffb0640b4
MrPushcarts <130241547+ipushthecarts@users.noreply.github.com>
2025-08-08 23:53:07 -0400

    Update info.js

--- info.js
+++ info.js
@@ -2,5 +2,5 @@ const infoData = [
     {"type":"tool", "link":"https://luphoria.com/netlog-policy-password-tool.html", "linkText":"Wifi Passord Getter", "otherContent":""},
     {"type":"guide", "link":"https://docs.titaniumnetwork.org/kajigs/downgrading/", "linkText":"Downgrading", "otherContent":"(May or may not work)"},
     {"type":"guide", "link":"https://discord.com/channels/419123358698045453/1291174869404225638", "linkText":"DAUB", "otherContent":"Update Blocker"},
-    {"type":"link", "link":"https://pbpc-n.com/hidden/imgonnafuckthegoat.html", "linkText":"IGFTG", "otherContent":""},
+    {"type":"link", "links":"https://pbpc-n.com/hidden/imgonnafuckthegoat.html", "linkText":"IGFTG", "otherContent":""},
 ];
